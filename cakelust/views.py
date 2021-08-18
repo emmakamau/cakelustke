1d1b5379b1dfd7b615c0508521c0f0d21ad3578b
emmakamau <42608367+emmakamau@users.noreply.github.com>
2021-08-18 06:49:19 +0300

    UI adjustment
    Adjusted navbar to fit fullpage

--- cakelust/views.py
+++ cakelust/views.py
@@ -1,16 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .models import *
+from .forms import *
 
 # Create your views here.
 def home(request):
 
+    gallery = Gallery.objects.all().order_by('-id')[:6]
+    review = Review.objects.all().order_by('-id')
+
+    form = reviewsForm()
+
+    if request.method == 'POST':
+        form = reviewsForm(request.POST)
+        if form.is_valid():
+            form = form.save()
+
+            messages.success(request, "Account created successfully")
+            return redirect('home')
+
     context = {
+        'gallery':gallery,
+        'form':form,
+        'review':review,
     }
     return render (request, 'cakelust/index.html', context=context)
 
 
 def gallery(request):
+    
+    gallery = Gallery.objects.all().order_by('-id')
 
     context = {
+        'gallery':gallery,
     }
     return render (request, 'cakelust/gallery.html', context=context)
 
@@ -24,6 +46,9 @@ def priceguide(request):
 
 def flavours(request):
 
+    cake = Cake.objects.all()
+
     context = {
+        'cake':cake,
     }
     return render (request, 'cakelust/flavours.html', context=context)
