43b1c651568bf2913c279c9632bcfad217aa89a7
emmakamau <42608367+emmakamau@users.noreply.github.com>
2021-08-21 12:29:48 +0300

    Update settings.py

--- cakelustke/settings.py
+++ cakelustke/settings.py
@@ -131,3 +131,4 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SESSION_COOKIE_SECURE = False
