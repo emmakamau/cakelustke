65bb6aa85210e8c04a6da47c885063f5d7d680be
emmakamau <42608367+emmakamau@users.noreply.github.com>
2021-09-26 09:14:25 +0300

    Update settings.py

--- cakelustke/settings.py
+++ cakelustke/settings.py
@@ -78,12 +78,13 @@ WSGI_APPLICATION = 'cakelustke.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': 'emmakamau',
-        'PASSWORD':'cake@21lust',
-        'HOST':'localhost',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'USER': 'emmakamau',
+        #'PASSWORD':'cake@21lust',
+        #'HOST':'localhost',
+        #'PORT':'5432',
     }
 }
 
